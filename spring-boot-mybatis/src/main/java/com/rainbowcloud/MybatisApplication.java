cf8d9cf7720a23a4c26d4f84553e7dd30439d241
DingYihang <1391767025@qq.com>
2024-03-25 13:24:07 +0800

    2023/3/25 编写Mybatis项目

--- spring-boot-mybatis/src/main/java/com/rainbowcloud/MybatisApplication.java
+++ spring-boot-mybatis/src/main/java/com/rainbowcloud/MybatisApplication.java
@@ -1,16 +1,23 @@
 package com.rainbowcloud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 /**
  * @author DingYihang
  */
 @MapperScan(basePackages ={"com.rainbowcloud.mapper"})
+@Slf4j
 @SpringBootApplication
 public class MybatisApplication {
     public static void main(String[] args) {
-       SpringApplication.run(MybatisApplication.class, args);
+        SpringApplication app = new SpringApplication(MybatisApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功");
+        log.info("swagger文档地址：http://localhost:{}/swagger-ui/index.html#/", env.getProperty("server.port"));
+        log.info("knife4j文档地址：http://localhost:{}/doc.html#/home", env.getProperty("server.port"));
     }
 }
